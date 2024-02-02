b754dedf5c90b092cea044072df3ff80bcb8407a
Ömer ALKAYA <alkayaomer1995@gmail.com>
2024-02-02 18:37:30 +0300

    Proje yüklendi

---
+++ ECommerceProject/client/src/scenes/checkout/Confirmation.jsx
@@ -0,0 +1,12 @@
+import{Box,Alert,AlertTitle} from '@mui/material';
+
+const Confirmation = () => {
+      return <Box m="90px auto" width="80%" height="50vh">
+        <Alert severity='success'>
+          <AlertTitle>Success</AlertTitle>
+          You have successfully made an Order- {" "}
+          <strong>Congrats on Making Your Purchases</strong>
+        </Alert>
+        </Box> 
+    };
+    export default Confirmation;
